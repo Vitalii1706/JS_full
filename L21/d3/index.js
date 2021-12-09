92403e7bb7f03caafc86bc829a31685629aa8b57
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-09 15:04:55 +0200

    1

--- L21/d3/index.js
+++ L21/d3/index.js
@@ -1,29 +1,22 @@
 const getTitle = () => {
   const elem = document.querySelector('.title');
+  console.dir(elem.textContent);
   return elem.textContent;
 };
-
-getTitle();
 
 const getDescription = () => {
   const elem = document.querySelector('.about');
   return elem.innerText;
 };
 
-getDescription();
-
 const getPlans = () => {
   const elem = document.querySelector('.plans');
   return elem.innerHTML;
 };
 
-getPlans();
-
 const getGoal = () => {
   const elem = document.querySelector('.goal');
   return elem.outerHTML;
 };
-
-getGoal();
 
 export { getTitle, getDescription, getPlans, getGoal };
