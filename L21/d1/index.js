4cda31ce5523e4d42fb8a2fd95ebff21acb7421c
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-09 12:52:25 +0200

    1

--- L21/d1/index.js
+++ L21/d1/index.js
@@ -1,36 +1,16 @@
-class Order {
-  constructor(price, city, type) {
-    this.dateCreated = new Date();
-    this.isConfirmed = false;
-    this.dateConfirmed = new Date();
-    this.price = price;
-    this.city = city;
-    this.type = type;
-    this.id = Math.random().toFixed(2);
-  }
+const getTitleElement = () => {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+};
 
-  checkPrice(price) {
-    if (this.price > 1000) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+getTitleElement();
 
-  confirmOrder() {
-    return (this.isConfirmed = true);
-  }
+const getInputElement = () => {
+  const inputElem = document.querySelector('input[type=text]');
+  console.dir(inputElem);
+  return inputElem;
+};
+getInputElement();
 
-  isValidType(type) {
-    if (this.type === 'Buy' || this.type === 'Sell') {
-      return true;
-    }
-    return false;
-  }
-}
-
-const Order1 = new Order(1500, 'Lviv', 'Buy');
-console.log(Order1);
-console.log(Order1.checkPrice());
-console.log(Order1.isValidType());
-console.log(Order1.confirmOrder());
+export { getTitleElement, getInputElement };
