408ca4d3313523b6a85c66db8efe0ded2b05b110
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-09 16:12:55 +0200

    1

--- L21/d4/index.js
+++ L21/d4/index.js
@@ -1,20 +1,8 @@
-class Wallet {
-  constructor() {
-    this.balance = 0;
-  }
+const getTitle = (text) => {
+  const elem = document.querySelector('.title');
+  elem.textContent = text;
+  console.log(elem);
+  return elem;
+};
 
-  getBalance() {
-    return this.balance;
-  }
-  deposit(amount) {
-    this.balance += amount;
-  }
-
-  withdraw(amount) {
-    if (amount > this.balance) {
-      console.log('No enough funds');
-      return;
-    }
-    this.balance -= amount;
-  }
-}
+export { getTitle };
