b72298875cd313b1c420ea1a8c430fc7c1d36d7f
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-09 13:12:33 +0200

    1

--- L21/d2/index.js
+++ L21/d2/index.js
@@ -1,68 +1,17 @@
-/* eslint-disable max-classes-per-file */
+const getitemsList = () => {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
+};
 
-class User {
-  constructor(id, name, sessionId) {
-    this._id = id;
-    this._name = name;
-    this._sessionId = sessionId;
-  }
+getitemsList();
 
-  get id() {
-    return this._id;
-  }
-  get name() {
-    return this._name;
-  }
+const getItemsArray = () => {
+  const nodeList = document.querySelectorAll('.tool');
+  const elementsArray = Array.from(nodeList);
+  console.dir(elementsArray);
+  return elementsArray;
+};
+getItemsArray();
 
-  get sessionId() {
-    return this._sessionId;
-  }
-}
-
-class UserRepository {
-  constructor(users) {
-    this._users = Object.freeze(users);
-  }
-
-  get users() {
-    return this._users;
-  }
-
-  getUserNames() {
-    return this._users.map((el) => el.name);
-  }
-
-  getUserIds() {
-    return this._users.map((el) => el.id);
-  }
-
-  getUserNameById(id) {
-    this._users.find((user) => {
-      if (user.id === id) return user.name;
-    });
-  }
-}
-
-// examples
-const user = new User('1', 'Tom', 'session-id');
-const user1 = new User('2', 'Edvard', 'session-id1');
-const user2 = new User('3', 'Ed', 'session-id2');
-const user3 = new User('4', 'Felix', 'session-id3');
-// получить свойства можем
-console.log(user); // ===> '1'
-console.log(user._name); // ===> 'Tom'
-console.log(user._sessionId); // ===> 'session-id'
-
-console.log(user2); // ===> '1'
-console.log(user2._name); // ===> 'Tom'
-console.log(user2._sessionId);
-const userRepository = new UserRepository([user, user1, user2, user3]);
-console.log(userRepository.getUserNames());
-console.log(userRepository.getUserIds());
-console.log(userRepository.getUserNameById(2));
-
-//console.log(UserRepository());
-
-// но изменить эти свойства нельзя
-// user.name = 'Bob'; // пытаемся изменить старое значение
-// console.log(user.name); // ===> 'Tom' - но изменение проигнорировано, так как setter отсутствует
+export { getitemsList, getitemsList };
