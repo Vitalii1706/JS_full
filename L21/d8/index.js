5c11972ad462f31c6b17ed22c8477ef3a125a59d
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-10 15:10:05 +0200

    1

--- L21/d8/index.js
+++ L21/d8/index.js
@@ -7,10 +7,10 @@ export const finishList = () => {
   const li8 = document.createElement('li');
   li8.textContent = 8;
   elemUl.append(li8);
-  li4 = document.createElement('li');
+  const li4 = document.createElement('li');
   li4.textContent = 4;
   elemLi.before(li4);
-  li6 = document.createElement('li');
+  const li6 = document.createElement('li');
   li6.textContent = 6;
   elemLi.after(li6);
 };
