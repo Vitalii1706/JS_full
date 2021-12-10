c1ecc82968b263ffa2ef73e4da0ac788016e155f
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-10 12:03:21 +0200

    1

--- L21/d5/index.js
+++ L21/d5/index.js
@@ -1,6 +1,7 @@
 const setButton = (buttonText) => {
   const elem = document.querySelector('body');
-  return (elem.innerHTML = buttonText);
+  return (elem.innerHTML = `<button>${buttonText}</button>`);
 };
 
+console.log(setButton('hello'));
 export { setButton };
