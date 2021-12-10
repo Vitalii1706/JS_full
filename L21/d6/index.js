5d9fc86353b2a54424223b6618d2cbb266a19a1e
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-10 12:11:38 +0200

    1

--- L21/d6/index.js
+++ L21/d6/index.js
@@ -1,17 +1,9 @@
-const getItemsList = () => {
-  const elementsList = document.querySelectorAll('.technology');
-  console.dir(elementsList);
-  return elementsList;
+const clearList = () => {
+  const element = document.querySelectorAll('.categories');
+  console.dir(element);
+  return (element.innerHTML = '');
 };
 
-getItemsList();
+clearList();
 
-const getItemsArray = () => {
-  const nodeList = document.querySelectorAll('.tool');
-  const elementsArray = Array.from(nodeList);
-  console.dir(elementsArray);
-  return elementsArray;
-};
-getItemsArray();
-
-export { getItemsList, getItemsArray };
+export { clearList };
