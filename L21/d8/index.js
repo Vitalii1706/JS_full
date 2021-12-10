9f9d409d8aded0041bb2c9c2412f0a20b987e02a
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-10 14:31:35 +0200

    1

--- L21/d8/index.js
+++ L21/d8/index.js
@@ -1,20 +1,18 @@
-class Wallet {
-  constructor() {
-    this.balance = 0;
-  }
-
-  getBalance() {
-    return this.balance;
-  }
-  deposit(amount) {
-    this.balance += amount;
-  }
-
-  withdraw(amount) {
-    if (amount > this.balance) {
-      console.log('No enough funds');
-      return;
-    }
-    this.balance -= amount;
-  }
-}
+const finishList = () => {
+  const elemUl = document.querySelector('.list');
+  const elemLi = document.querySelector('.special');
+  const li1 = document.createElement('li');
+  li1.textContent = 1;
+  elemUl.prepend(li1);
+  const li8 = document.createElement('li');
+  li8.textContent = 8;
+  elemUl.append(li8);
+  li4 = document.createElement('li');
+  li4.textContent = 4;
+  elemLi.before(li4);
+  li6 = document.createElement('li');
+  li6.textContent = 6;
+  elemLi.after(li6);
+};
+finishList();
+export { finishList };
