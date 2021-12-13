15446870593fdf1cbcae802f815d24cf4615735b
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-13 16:59:27 +0200

    1

--- L23/les2/index.js
+++ L23/les2/index.js
@@ -1,8 +1,61 @@
-const buttElemts = Array.from(document.querySelectorAll('.pagination__page'));
-console.log(buttElemts);
+const generateNumbersRange = (from, to) => {
+  const result = [];
 
-const handleClick = (event) => {
-  console.log(event.target.dataset.pageNumber);
+  for (let i = from; i <= to; i++) {
+    result.push(i);
+  }
+  return result;
 };
 
-buttElemts.map((btn) => btn.addEventListener('click', handleClick));
+const getLineSeats = () =>
+  generateNumbersRange(1, 10)
+    .map(
+      (seatNumber) =>
+        `<div class="sector__seat" data-seat-number="${seatNumber}"></div>`
+    )
+    .join('');
+
+const getSectorLines = () => {
+  const seatsString = getLineSeats();
+  return generateNumbersRange(1, 10)
+    .map(
+      (lineNumber) =>
+        `<div class="sector__line" data-line-number="${lineNumber}">${seatsString}</div>`
+    )
+    .join('');
+};
+
+const arenaElem = document.querySelector('.arena');
+
+const renderArena = () => {
+  const linesString = getSectorLines();
+
+  const sectorSrting = generateNumbersRange(1, 3)
+    .map(
+      (sectorNumber) =>
+        `<div class="sector" data-sector-number="${sectorNumber}">${linesString}</div>`
+    )
+    .join('');
+
+  arenaElem.innerHTML = sectorSrting;
+};
+
+const onSeatSelect = (event) => {
+  const isSeat = event.target.classList.contains('sector__seat');
+
+  if (!isSeat) {
+    return;
+  }
+
+  const seatNumber = event.target.dataset.seatNumber;
+  const lineNumber = event.target.closest('.sector__line').dataset.lineNumber;
+  const sectorNumber = event.target.closest('.sector').dataset.sectorNumber;
+
+  const selectedSeatElem = document.querySelector('.board__selected-seat');
+
+  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
+};
+
+arenaElem.addEventListener('click', onSeatSelect);
+
+renderArena();
