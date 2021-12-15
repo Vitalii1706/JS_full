1f6a6ae008e66715f5b4cb18151aa24786973518
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-15 13:39:01 +0200

    3

--- L27/les1/index.js
+++ L27/les1/index.js
@@ -1,8 +1,3 @@
-localStorage.clear();
-localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-localStorage.setItem('name', JSON.stringify('Tom'));
-localStorage.setItem('age', JSON.stringify(17));
-
 const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
     return {
