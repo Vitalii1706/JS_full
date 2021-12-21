3f85b44e44b30ef21f47829c7cd702b2cfd1594e
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-21 13:34:01 +0200

    1

--- L29/les3/index.js
+++ L29/les3/index.js
@@ -1,5 +1,27 @@
-export const mult = (a) => (b) => a * b;
-export const twice = mult(2);
-export const triple = mult(3);
+"use strict";
 
-//export { mult, twice, triple};
+/**
+ * @return {undefined}
+ */
+
+const printing = () => {
+  console.log(1);
+
+  setTimeout(function () {
+    console.log(5);
+  }, 1000);
+
+  console.log(2);
+
+  setTimeout(function () {
+    console.log(4);
+  }, 0);
+
+  setTimeout(function () {
+    console.log(6);
+  }, 2000);
+
+  console.log(3);
+};
+
+printing();
