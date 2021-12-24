f996e9afbd17ddcf7a8effb7768766b849c9845c
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-24 16:30:09 +0200

    1

--- L33/taskHome1/index.js
+++ L33/taskHome1/index.js
@@ -1,13 +1,19 @@
-/*
- * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
- */
+const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
 
-const makePromise = (value) => {
-  return Promise.resolve(value);
-};
+export function getTasksList() {
+  return fetch(baseUrl).then((response) => response.json());
+}
+
+export function getTaskById(taskId) {
+  return fetch(`${baseUrl}/${taskId}`).then((response) => response.json());
+}
+
 /*
- * пример использования
- */
-makePromise(17).then((number) => {
-  console.log(number); // 17
+getTasksList().then((tasksList) => {
+  console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
 });
+
+getTaskById("2").then((taskData) => {
+  console.log(taskData); // {'id':'2', 'done':true ... }
+});
+*/
