e317761df9adab3aee70bb33847a49a61dd08237
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-24 14:24:39 +0200

    1

---
+++ L32/task1/index.js
@@ -0,0 +1,27 @@
+const getValueWithDelay = (value, delay) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(value);
+      resolve(value);
+    }, delay);
+  });
+
+const asyncNum1 = getValueWithDelay(56, 1000);
+const asyncNum2 = getValueWithDelay(14, 2000);
+const asyncNum3 = getValueWithDelay(10, 3000);
+
+const getSum = (number) => {
+  number
+    .filter((value) => !isNaN(value))
+    .reduce((acc, num) => acc + Number(num), 0);
+};
+
+const asyncSum = (...asyncNumbers) => {
+  return Promise.all(asyncNumbers)
+    .then((numbers) => getSum(numbers))
+    .catch(() => Promise.reject(new Error("Can't calculate\")));
+};
+
+asyncSum(asyncNum1, Promise.reject(new Error(`err`)), asyncNum3).then(
+  (result) => console(result)
+);
