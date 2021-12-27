a1140c6885fa8fe6c356391fdb495ac7419e9edd
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-27 19:10:01 +0200

    1

--- L34/taskHome1/index.js
+++ L34/taskHome1/index.js
@@ -1,19 +1,72 @@
-const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
+const baseUrl = "https://61c732c3903185001754733f.mockapi.io/api/v1/users";
 
-export function getTasksList() {
-  return fetch(baseUrl).then((response) => response.json());
+export function getUsersList() {
+  return fetch(baseUrl).then((resoponse) => resoponse.json());
 }
 
-export function getTaskById(taskId) {
-  return fetch(`${baseUrl}/${taskId}`).then((response) => response.json());
+export function getUserById(userId) {
+  return fetch(`${baseUrl}/${userId}`).then((resoponse) => resoponse.json());
 }
 
+export function createUser(userData) {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+export function deleteUser(userId) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: "DELETE",
+  });
+}
+
+export function updateUser(userId, userData) {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+// examples
 /*
-getTasksList().then((tasksList) => {
-  console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
+getUsersList().then((users) => {
+  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
 });
 
-getTaskById("2").then((taskData) => {
-  console.log(taskData); // {'id':'2', 'done':true ... }
+getUserById("3").then((userData) => {
+  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+});
+
+const newUserData = {
+  email: "ivan@email.com",
+  firstName: "Ivan",
+  lastName: "Murat",
+  age: 40,
+};
+
+createUser(newUserData).then(() => {
+  console.log("User created");
+});
+
+const updatedUserData = {
+  email: "new@email.com",
+  firstName: "John",
+  lastName: "Doe",
+  age: 17,
+};
+
+updateUser("1", updatedUserData).then(() => {
+  console.log("User updated");
+});
+
+deleteUser("9").then(() => {
+  console.log("User updated");
 });
 */
--- L34/taskHome2/index.js
+++ L34/taskHome2/index.js
@@ -1,10 +1,42 @@
-const commitUrl =
-  "https://api.github.com/repos/USERID/REPOID/commits?per_page=100";
+const baseUrl =
+  "https://61c732c3903185001754733f.mockapi.io/api/v1/create_users";
 
-const getMostActiveDevs = () => {
-  return fetch(commitUrl)
-    .then((response) => response.json())
-    .then((result) => console.log(result));
+const submBtnElem = document.querySelector(".submit-button");
+const formElem = document.querySelector(".login-form");
+
+const validation = () => {
+  const isValide = formElem.reportValidity();
+  submBtnElem.disabled = !isValide;
+  return submBtnElem.disabled;
 };
 
-getMostActiveDevs();
+function addNewUser(user) {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(user),
+  });
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const formData = [...new FormData(formElem)].reduce(
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {}
+  );
+  addNewUser(formData)
+    .then((response) => response.json())
+    .then((user) => {
+      alert(JSON.stringify(user));
+      formElem.reset();
+      submBtnElem.disabled = true;
+    })
+    .catch(() => {
+      submBtnElem.disabled = true;
+    });
+}
+
+formElem.addEventListener("submit", onFormSubmit);
+formElem.addEventListener("click", validation);
